3893d690e72a418cde4c597d695cb3580f560432
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-12-16 08:56:29 +0000

    [Add]Bootstrap/enum/FontAwesome/kaminari追加

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,5 @@
 class OrderItem < ApplicationRecord
+  enum crafting_status_method: { start_not_possible: 0,production_pending: 1,
+                                 in_production: 2, production_complete: 3 }
+                                 
 end
