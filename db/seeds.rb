ff9d9ca97f40cd419aaea0ef6f38ab351bd567cf
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-18 02:59:09 +0000

    [update] 管理者でログインできる

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Character.create(name: 'Luke', movie: movies.first)
+# Admin.create([{ email: 'test@mail.com' }, { password: 'testtest' }])
