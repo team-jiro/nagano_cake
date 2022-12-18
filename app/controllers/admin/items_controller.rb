9d00f0384766d0ce6504ab35fb9811743d3d488d
EC2 Default User <ec2-user@ip-172-31-16-215.ap-northeast-1.compute.internal>
2022-12-18 04:10:01 +0000

    [ADD]admin/items_view 作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,40 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  def new
+    @item = Item.new
+    @items = Item.all
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = " 作成されました。"
+      redirect_to admin_item_path(@item.id)
+    else
+      flash[:alert] = "エラーが発生しました。"
+      render :new
+    end
+  
+  
+　def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @categories = Category.all
+    @item = Items.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    flash[:notice] = "編集完了。"
+    redirect_to admin_item_path(item.id)
+  end
+  
+   private
+  def item_params
+    params.require(:item).permit(:name, :caption, :price, :image, :category_id, :is_active)
+  end
+
 end
