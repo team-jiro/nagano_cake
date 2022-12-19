e79dda51aba1180b6b72fef126162b53b80ad020
EC2 Default User <ec2-user@ip-172-31-16-215.ap-northeast-1.compute.internal>
2022-12-19 01:18:08 +0000

    [ADD]order作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,32 @@
 class Admin::OrdersController < ApplicationController
+  
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @total = 0
+    @order_items.each do |order_item|
+      @total += order_item.item.add_tax_price * order_item.amount
+    end
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+
+    if @order.status == "payment_cofirm"
+      flash[:notice] = "作り始めよう！"
+      @order.order_items.update(making_status: "waiting")
+    end
+
+    if @order.status == "shipped"
+      flash[:notice] = "頑張ったね！"
+    end
+    redirect_to request.referer
+  end
+  
+private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
