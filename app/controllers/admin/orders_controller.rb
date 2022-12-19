a05393bc14732d5f8c2cb7ee8ae14e3e7033b9ee
EC2 Default User <ec2-user@ip-172-31-16-215.ap-northeast-1.compute.internal>
2022-12-19 02:02:12 +0000

    [ADD]admin/orders/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,14 +12,13 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-
     if @order.status == "payment_cofirm"
-      flash[:notice] = "作り始めよう！"
-      @order.order_items.update(making_status: "waiting")
+      flash[:notice] = "完了"
+      @order.order_items.update(crafting_status: "waiting")
     end
 
     if @order.status == "shipped"
-      flash[:notice] = "頑張ったね！"
+      flash[:notice] = "作成完了"
     end
     redirect_to request.referer
   end
