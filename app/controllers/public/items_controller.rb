0b9e9f3f18badd75b6c89187808afc725dabf3fc
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-22 04:34:37 +0000

    [fix] コントローラ：アクセス権限の修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,5 @@
 class Public::ItemsController < ApplicationController
-  before_action :authenticate_customer!
-  
+
   def index
     @items = Item.all
   end
@@ -9,5 +8,5 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
-  
+
 end
