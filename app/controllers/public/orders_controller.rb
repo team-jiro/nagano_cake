2bb640eef8a2c134d7661ec9afa87acee44e21fe
EC2 Default User <ec2-user@ip-172-31-16-215.ap-northeast-1.compute.internal>
2022-12-22 05:57:08 +0000

    [FIX]public/order/showレイアウト

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -73,6 +73,12 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @total = 0
+    @shipping_cost = 800
+    @order_items.each do |order_item|
+    @total += order_item.tax_included_price * order_item.amount
+    end
 
   end
 private
