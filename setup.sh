68daf8c9042256a4f2099a00e8f240d75b7d48ca
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-24 02:24:10 +0000

    [fix] setup.sh：「rails db:seed」の追加

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ bundle install
 rails webpacker:install
 rails webpacker:compile
 rails db:migrate
+rails db:seed
 echo 'Nagano Cake has been successfully installed !'
+echo 'To start registering products, please access the following URL and log in as an administrator.'
+echo '(By default, the email address is "test@mail.com" and the password is "testtest")'
+echo 'https:// "domain name"/admin/sign_in'
